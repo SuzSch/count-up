a188f56b126c586c13197d9a08824e8885d92aa9
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-09 07:58:48 -0700

    add files.

---
+++ README.md
@@ -0,0 +1,10 @@
+Describe: Count and Multiply By form()
+
+Test "It should take any number and divide it by any number and show result."
+Code: 
+const enteredNum = users first input;
+const dividedBy = user second input;
+equalsTo (user first input / users second input);
+Expected output total of first number divided by the second number
+
+
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//Business Logic
+
+
+
+
+for (let index = "start-number"; index < "star-number".length; index / "multiple-number") {
+  const element = array[index];
+  total;
+  console.log
+}
+
+
+
+
+
+
+
+
+//= ["start-number" / "multiple-number"];
+
+
+//UI Logic
+document.querySelector("form").onsubmit = function () {
+  const number1 = document.getElementById("start-number").value;
+  const number2 = document.getElementById("multiple-number").value;
+};
+
+
+
+
+
+
